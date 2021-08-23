2f097db23281712260065f633d53b54752975392
orsel <orselv99@gmail.com>
2021-08-24 08:54:22 +0900

    quill 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import { useState, useRef, useMemo } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 function App() {
+  const QuillRef = useRef<ReactQuill>();
+  const [contents, setContents] = useState('');
+
+  // https://velog.io/@sklsh917/React-Quill%EC%9D%84-%ED%99%9C%EC%9A%A9%ED%95%98%EC%97%AC-%EA%B2%8C%EC%8B%9C%ED%8C%90-%EB%A7%8C%EB%93%A4%EA%B8%B0with-TypeScript
+  // useMemo를 사용하지 않으면, 키를 입력할 때마다, imageHandler 때문에 focus가 계속 풀림
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+          [{ size: ['small', false, 'large', 'huge'] }, { color: [] }],
+          [
+            { list: 'ordered' },
+            { list: 'bullet' },
+            { indent: '-1' },
+            { indent: '+1' },
+            { align: [] },
+          ],
+          ['image', 'video'],
+        ],
+        handlers: {},
+      },
+    }),
+    []
+  );
+
   return (
-    <div>
-      <p>Reaching.For.You</p>
-    </div>
+    <>
+      <div>
+        <ReactQuill
+          ref={(element) => {
+            if (element !== null) {
+              QuillRef.current = element;
+            }
+          }}
+          value={contents}
+          theme='snow'
+          modules={modules}
+          onChange={setContents}
+          placeholder='내용을 입력해주세요.'></ReactQuill>
+      </div>
+    </>
   );
 }
 
